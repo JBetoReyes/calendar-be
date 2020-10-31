b38755cbfa62a8f602d4663af2895300a0ffcd45
beto <jose.a.reyes.quevedo@accenture.com>
2020-10-30 22:47:15 -0600

    adding auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,6 +2,23 @@ import express from 'express';
 
 export const AuthRouter = express.Router();
 
-AuthRouter.get('/', (req, res) => {
-  res.json({ok: true});
+AuthRouter.post('/new', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'register',
+  });
+});
+
+AuthRouter.post('/', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'login',
+  });
+});
+
+AuthRouter.get('/renew', (req, res) => {
+  res.json({
+    ok: true,
+    msg: 'renew',
+  });
 });
