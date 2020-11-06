e91387bcc9bd71e3206769173baab939b3e43659
beto <josereyes55@hotmail.com>
2020-11-05 20:51:34 -0600

    adding password as required

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -13,6 +13,7 @@ export const createUserMiddlewares = [
 export const loginMiddlewares = [
   check('name', 'Name is required').notEmpty(),
   check('email', 'Email is required').notEmpty().isEmail(),
+  check('password', 'Password is required').notEmpty().isLength({min: 6}),
   fieldValidator,
 ];
 
