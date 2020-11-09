e5e78fdbf7f1e53552e7be279b2b2add9e614f89
beto <josereyes55@hotmail.com>
2020-11-08 18:23:01 -0600

    gets rid of the name validation

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -11,7 +11,6 @@ export const createUserMiddlewares = [
 ];
 
 export const loginMiddlewares = [
-  check('name', 'Name is required').notEmpty(),
   check('email', 'Email is required').notEmpty().isEmail(),
   check('password', 'Password is required').notEmpty().isLength({min: 6}),
   fieldValidator,
